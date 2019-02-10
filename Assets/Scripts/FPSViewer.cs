e5fc15a2c243b41e9c9a0e66bf033cfb7ba08f8c
u16kuma <u16.kuma@gmail.com>
2019-02-10 20:12:48 +0900

    FPSViewerを追加

---
+++ Assets/Scripts/FPSViewer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSViewer : MonoBehaviour
+{
+	[SerializeField]
+	private Text text;
+
+	int frameCount;
+	float prevTime;
+
+	void Start()
+	{
+		frameCount = 0;
+		prevTime = 0.0f;
+	}
+
+	void Update()
+	{
+		++frameCount;
+		float time = Time.realtimeSinceStartup - prevTime;
+
+		if (time >= 0.5f)
+		{
+			text.text = string.Format("{0:0.00} fps", frameCount / time);
+
+			frameCount = 0;
+			prevTime = Time.realtimeSinceStartup;
+		}
+	}
+}
